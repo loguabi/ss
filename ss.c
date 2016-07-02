01c4d6371428452e6cdaa8d69cc2de4cb61ebc01
logitha <loga848.456@gmail.com>
2016-07-02 16:28:13 +0530

    Create ss.c

---
+++ ss.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int x,y,z,w[80];
+printf("Enter the number");
+scanf("%d",&x);
+for(z=0;z<x;z++);
+{
+scanf("%d",&w[x]);
+scanf("%d",&y);
+printf("%d",w[y]);
+}
+return 0;
+}
